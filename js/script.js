f9e74a573c8448bad2861c760445df484366e7d4
Rodrigo Nunes <imrodrigon@gmail.com>
2022-05-21 17:43:48 -0400

    delete fetch

--- js/script.js
+++ js/script.js
@@ -1,41 +1,62 @@
-async function fetchData() {
-  const response = await fetch('../data.json');
-  const data = await response.json();
-  const section = document.querySelector('.section');
-  data.forEach((item) => {
-    const div = document.createElement('div');
+const data = [
+  {
+    day: 'mon',
+    amount: 17.45,
+  },
+  {
+    day: 'tue',
+    amount: 34.91,
+  },
+  {
+    day: 'wed',
+    amount: 52.36,
+  },
+  {
+    day: 'thu',
+    amount: 31.07,
+  },
+  {
+    day: 'fri',
+    amount: 23.39,
+  },
+  {
+    day: 'sat',
+    amount: 43.28,
+  },
+  {
+    day: 'sun',
+    amount: 25.48,
+  },
+];
 
-    div.innerHTML = `
+const section = document.querySelector('.section');
+data.forEach((item) => {
+  const div = document.createElement('div');
+
+  div.innerHTML = `
               <span class="amount">${item.amount}</span>
               <div class="bar"></div>
               <span class="day">${item.day}</span>
           `;
-    section.appendChild(div);
-  });
-}
+  section.appendChild(div);
+});
 
-async function setHeightBar() {
-  await fetchData();
+const bar = document.querySelectorAll('.bar');
+const amount = document.querySelectorAll('.amount');
+const amounts = [];
 
-  const bar = document.querySelectorAll('.bar');
-  const amount = document.querySelectorAll('.amount');
-  const amounts = [];
+amount.forEach((item) => amounts.push(item.textContent));
 
-  amount.forEach((item) => amounts.push(item.textContent));
+const maxValue = Math.max(...amounts);
 
-  const maxValue = Math.max(...amounts);
+bar.forEach((item, index) => {
+  item.style = `height: ${Number(amount[index].innerText) * 2}px`;
+  if (amount[index].textContent == maxValue) {
+    item.classList.add('maxValue');
+    item.parentElement.children[0].style.opacity = 1;
+  }
+});
 
-  bar.forEach((item, index) => {
-    item.style = `height: ${Number(amount[index].innerText) * 2}px`;
-    if (amount[index].textContent == maxValue) {
-      item.classList.add('maxValue');
-      item.parentElement.children[0].style.opacity = 1;
-    }
-  });
+const total = document.querySelector('.total');
 
-  const total = document.querySelector('.total');
-
-  total.innerText = amounts.reduce((total, valor) => total + Number(valor), 0);
-}
-
-setHeightBar();
+total.innerText = amounts.reduce((total, valor) => total + Number(valor), 0);
